0d8f8e6bab1e294ae0108717a6b2fc72612db7c5
Benjamin Peterson <benjamin@dropbox.com>
2019-12-26 22:28:13 +0000

    py3 go env.py.
    GitOrigin-RevId: b6e07938bd450ff52af8ff9187ae455edb296102

--- build_tools/go/env.py
+++ build_tools/go/env.py
@@ -1,11 +1,12 @@
-# mypy: skip-file
-
 import os
+
+from typing import Dict
 
 from build_tools import bazel_utils
 
 
 def make_go_env():
+    # type: () -> Dict[str, str]
     # TODO Respect args to bzl to use the proper default runtime.
     ws = bazel_utils.find_workspace()
     bazel_ws_root = "bazel-" + os.path.basename(ws)
@@ -15,5 +16,5 @@ def make_go_env():
         "GOROOT": GOROOT,
         "GOPATH": os.path.join(bazel_utils.find_workspace(), "go"),
         # go-errcheck behaves incorrectly if $GOROOT/bin is not added to $PATH
-        "PATH": os.path.join(GOROOT, "bin") + ":" + os.getenv("PATH"),
+        "PATH": os.path.join(GOROOT, "bin") + ":" + os.getenv("PATH", ""),
     }
