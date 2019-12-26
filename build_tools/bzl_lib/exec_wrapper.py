a7ecedef4122248e36b14d897eabb86bedcee1d4
Benjamin Peterson <benjamin@dropbox.com>
2019-12-26 23:23:06 +0000

    Modernize exec_wrapper and run.
    GitOrigin-RevId: 89fc31e9defa9d0b006627292909e646a396914b

--- build_tools/bzl_lib/exec_wrapper.py
+++ build_tools/bzl_lib/exec_wrapper.py
@@ -1,14 +1,14 @@
-# mypy: allow-untyped-defs
-
 """
 Helper functions that replace os.exec* functions. This is mostly for debugging
 and metrics purposes.
 """
+from __future__ import absolute_import, print_function
+
 import os
 import pipes
 import sys
 
-from typing import Any, List, Text
+from typing import Any, List, Mapping, Text
 
 from build_tools.bzl_lib import metrics
 
@@ -17,8 +17,9 @@ def execv(binary, args):
     # type: (Text, List[Any]) -> None
     metrics.report_metrics()
     if os.getenv("BZL_DEBUG"):
-        print >>sys.stderr, "exec: {} {}".format(
-            binary, " ".join(pipes.quote(s) for s in args[1:])
+        print(
+            "exec: {} {}".format(binary, " ".join(pipes.quote(s) for s in args[1:])),
+            file=sys.stderr,
         )
     os.execv(binary, args)
 
@@ -27,27 +28,36 @@ def execvp(binary, args):
     # type: (Text, List[str]) -> None
     metrics.report_metrics()
     if os.getenv("BZL_DEBUG"):
-        print >>sys.stderr, "exec: {} {}".format(
-            binary, " ".join(pipes.quote(s) for s in args[1:])
+        print(
+            "exec: {} {}".format(binary, " ".join(pipes.quote(s) for s in args[1:])),
+            file=sys.stderr,
         )
     os.execvp(binary, args)
 
 
 def execve(binary, args, env):
+    # type: (Text, List[str], Mapping[str, str]) -> None
     metrics.report_metrics()
     if os.getenv("BZL_DEBUG"):
-        print >>sys.stderr, "exec: {env} {args}".format(
-            env=" ".join("{}={}".format(k, pipes.quote(v)) for k, v in env.iteritems()),
-            args=binary + " " + " ".join(pipes.quote(s) for s in args[1:]),
+        print(
+            "exec: {env} {args}".format(
+                env=" ".join("{}={}".format(k, pipes.quote(v)) for k, v in env.items()),
+                args=binary + " " + " ".join(pipes.quote(s) for s in args[1:]),
+            ),
+            file=sys.stderr,
         )
     os.execve(binary, args, env)
 
 
 def execvpe(binary, args, env):
+    # type: (Text, List[str], Mapping[str, str]) -> None
     metrics.report_metrics()
     if os.getenv("BZL_DEBUG"):
-        print >>sys.stderr, "exec: {env} {args}".format(
-            env=" ".join("{}={}".format(k, pipes.quote(v)) for k, v in env.iteritems()),
-            args=binary + " " + " ".join(pipes.quote(s) for s in args[1:]),
+        print(
+            "exec: {env} {args}".format(
+                env=" ".join("{}={}".format(k, pipes.quote(v)) for k, v in env.items()),
+                args=binary + " " + " ".join(pipes.quote(s) for s in args[1:]),
+            ),
+            file=sys.stderr,
         )
     os.execvpe(binary, args, env)
--- build_tools/bzl_lib/run.py
+++ build_tools/bzl_lib/run.py
@@ -1,19 +1,21 @@
+from __future__ import print_function
+
 import subprocess
 import sys
 
-from typing import Any
+from typing import List, Text
 
 from build_tools.go.env import make_go_env
 
 
 def run_cmd(cmd, verbose=False):
-    # type: (Any, bool) -> str
+    # type: (List[str], bool) -> Text
     env = make_go_env()
-    env_args = ["%s=%s" % x for x in sorted(env.iteritems())]
+    env_args = ["%s=%s" % x for x in sorted(env.items())]
     if verbose:
-        print >>sys.stderr, "exec:", " ".join(env_args + cmd)
+        print("exec:", " ".join(env_args + cmd), file=sys.stderr)
     try:
-        output = subprocess.check_output(cmd, env=env)
+        output = subprocess.check_output(cmd, env=env).decode("ascii")
     except subprocess.CalledProcessError:
         sys.exit(1)
 
