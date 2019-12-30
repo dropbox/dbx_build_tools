df8b2bd6cd83953932246007d910736a385c33a7
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-12-30 16:22:26 +0000

    Add a bunch of files to mypy build at a cost of few type: ignores
    Summary: Moving towards the goal of disabling `--ignore-missing-imports` in server mypy build. This auto-generated diff has no semantic changes except import sort.
    
    Test Plan: Changes (+ mypy daemon locally)
    
    GitOrigin-RevId: 9af6997686bc0784790d9d28ad9aaa7225e52068

--- build_tools/chronic.py
+++ build_tools/chronic.py
@@ -1,4 +1,3 @@
-# mypy: skip-file
 # This script runs a command and drops all output unless the command fails.
 # It's useful for wrapping spammy scripts in build steps.
 #
@@ -15,7 +14,7 @@ if len(sys.argv) == 1:
 try:
     subprocess.check_output(sys.argv[1:], stderr=subprocess.STDOUT)
 except subprocess.CalledProcessError as e:
-    sys.stderr.buffer.write(e.output)
+    sys.stderr.buffer.write(e.output)  # type: ignore[attr-defined]
     if e.returncode < 0:
         # subprocess killed by signal
         sys.exit(1)
