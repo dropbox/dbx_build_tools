78d8072f7a35d88bcddfcf43bd765cf08eb698c5
Jared Hance <jhance@dropbox.com>
2020-01-31 10:29:39 -0800

    Batch BUILD merging in bzl gen
    Summary:
    Shelling out is expensive. I played with doing it with asyncio.subprocess but it didn't
    help much (process creation is too expensive). And batching is simpler to implement anyway.
    It doesn't seem like we need to bother parallelizing on the go side (if we do, we need
    to modify the python side a bit to not rely on ordering)
    
    The maximum batch size is simply so that we don't trip the os limit for maximum # of args
    passed to the process, although I'm not sure that this is a problem in practice.
    
    GitOrigin-RevId: d198b7591e73ce07ae2cf30d6d794a1e19ed2b24

--- build_tools/bzl_lib/gazel.py
+++ build_tools/bzl_lib/gazel.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 
 import os
-import shutil
 
 from collections import defaultdict
 
@@ -169,11 +168,33 @@ def regenerate_build_files(
 def merge_generated_build_files(generated_files):
     buildfmt_path = runfiles.data_path("@dbx_build_tools//build_tools/buildfmt")
 
+    merge_batch = []
+    files_to_remove = set()
+
     for dirpath, intermediate_build_files in generated_files.items():
         # if `intermediate_build_files` contains only 'BUILD', it means
         # exactly one build generator generates the BUILD file directly
         # in the directory and there's no need to merge it
         output_file = os.path.join(dirpath, "BUILD")
+        alt_output_file = os.path.join(dirpath, "BUILD.bazel")
+
+        # Extra crap to deal with OSX's shitty case insensitive file system.
+        build_names = []
+        for name in os.listdir(dirpath):
+            if name.lower() == "build":
+                build_names.append(name)
+
+        if len(build_names) > 1:
+            print(
+                (
+                    "WARNING: %s renamed to BUILD.bazel due to case "
+                    "insensitivity name conflict" % output_file
+                )
+            )
+            output_file = alt_output_file
+        elif os.path.isfile(alt_output_file):
+            print("WARNING: %s removed" % alt_output_file)
+            os.remove(alt_output_file)
 
         if intermediate_build_files == [output_file]:
             # always buildfmt even if not merging generated files
@@ -189,34 +210,21 @@ def merge_generated_build_files(generated_files):
             fd.write(HEADER)
 
         for filename in intermediate_build_files:
-            build_merge.merge_build_files(output_file, filename, output_file)
-            os.remove(filename)
+            merge_batch.append((output_file, filename, output_file))
+            files_to_remove.add(filename)
 
         annotation_file = os.path.join(dirpath, "BUILD.in")
 
         if os.path.isfile(annotation_file):
-            build_merge.merge_build_files(output_file, annotation_file, output_file)
+            merge_batch.append((output_file, annotation_file, output_file))
         else:
             annotation_file = os.path.join(dirpath, "BUILD.in-gen-proto~")
 
             if os.path.isfile(annotation_file):
-                build_merge.merge_build_files(output_file, annotation_file, output_file)
+                merge_batch.append((output_file, annotation_file, output_file))
 
-        # Extra crap to deal with OSX's shitty case insensitive file system.
-        build_names = []
-        for name in os.listdir(dirpath):
-            if name.lower() == "build":
-                build_names.append(name)
-
-        alt_output_file = os.path.join(dirpath, "BUILD.bazel")
-        if len(build_names) > 1:
-            print(
-                (
-                    "WARNING: %s renamed to BUILD.bazel due to case "
-                    "insensitivity name conflict" % output_file
-                )
-            )
-            shutil.move(output_file, alt_output_file)
-        elif os.path.isfile(alt_output_file):
-            print("WARNING: %s removed" % alt_output_file)
-            os.remove(alt_output_file)
+    # NOTE(jhance) Build merge merges in order, and this relies on that, since some of these
+    # files in the batch have the same output file.
+    build_merge.batch_merge_build_files(merge_batch)
+    for f in files_to_remove:
+        os.remove(f)
