b4f8de8738e675d15f6114a66f1c450cd3b09a09
Dave Zbarsky <zbarsky@dropbox.com>
2020-03-09 04:08:53 +0000

    Completely remove zip support from importlib_metadata
    Summary: Saves another 150ms at startup
    GitOrigin-RevId: bfe78861de157270085661887db3befbf8c5f935

--- pip/pytest/main.py
+++ pip/pytest/main.py
@@ -1,31 +1,46 @@
+# isort:skip_file
+# This file is carefully crafted to import things in the right order
 import sys
 
 if sys.argv[1] == "--profile":
     sys.argv.pop(1)
     from dropbox.testutils.plugins import dbxperf
+
     stop_profiling = dbxperf.start()
 else:
     stop_profiling = None
 
+# The only zip in our path is the stdlib, which won't have distributions.
+# Neuter importlib_metadata's support for zip files to avoid unzipping stdlib
+# repeatedly, saving around 1s on pytest startup time.
+#
+# Our journey is made more complicated by importlib_metadata's insistence on
+# computing it's own __version__ by checking its own distribution at import time.
+# Thus we first must no-op `zipp` to make the importlib_metadata import behave,
+# then we can properly neuter importlib_metadata and restore `zipp` support.
+#
+# Once we fully switch to Py3 and use importlib.metadata from stdlib, this can
+# be simplifed as the __version__ complications will be moot.
+class FakeZipPath(object):
+    def __init__(self, *args):  # type: ignore[no-untyped-def]
+        self.root = self
+
+    def namelist(self):  # type: ignore[no-untyped-def]
+        return []
+
+
+import zipp  # type: ignore[import]
+
+zippPath = zipp.Path
+zipp.Path = FakeZipPath
+
+from importlib_metadata import FastPath  # type: ignore[import]
+
+FastPath.zip_children = lambda _: []
+zipp.Path = zippPath
+
 import os
-
 import pytest
-
-from importlib_metadata import MetadataPathFinder  # type: ignore[import]
-
-
-# The only zip dist we use is the stdlib, which won't have the packages we are looking for.
-# Skipping that path saves around 1s on pytest startup time.
-class MetadataImporter(MetadataPathFinder):
-    def find_distributions(self, context=MetadataPathFinder.Context()):  # type: ignore[no-untyped-def]
-        path = [p for p in context.path if not p.endswith(".zip")]
-        return super(MetadataImporter, self).find_distributions(
-            MetadataPathFinder.Context(name=context.name, path=path)
-        )
-
-
-sys.meta_path = [r for r in sys.meta_path if not hasattr(r, "find_distributions")]
-sys.meta_path.insert(0, MetadataImporter())
 
 __file__ = "py.test"
 sys.argv[0] = sys.argv[0].replace("main.py", "py.test")
