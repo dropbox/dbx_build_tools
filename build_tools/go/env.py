da72c9645bfa67bee031b06ef1562a99dbb6cacf
Genghis Chau <sakura@dropbox.com>
2020-06-18 23:28:40 +0000

    [bzl] Do not require GOROOT to exist for `bzl tool`
    GitOrigin-RevId: 57014cd6303958cbd85d775b129182049c9c31b0

--- build_tools/go/env.py
+++ build_tools/go/env.py
@@ -6,32 +6,36 @@ from typing import Dict
 from build_tools import bazel_utils
 
 
-def ensure_goroot_exists(goroot):
+def _ensure_goroot_exists(goroot):
     # type: (str) -> None
-    """Checks if the GOROOT specified actually exists, and tries to populate it
-    if it doesn't exist."""
-    if not goroot or os.path.isdir(goroot):
-        return
-
+    """Tries to populate GOROOT and verify that it exists."""
     # Doesn't actually build anything, but should populate the Go toolchain.
-    subprocess.check_call(["bazel", "build", "@dbx_build_tools//build_tools/go:go1.12"])
+    subprocess.run(
+        ["bazel", "build", "@dbx_build_tools//build_tools/go:go1.12"], check=True, capture_output=True
+    )
     assert os.path.isdir(goroot), "Could not populate GOROOT"
 
 
-def make_go_env():
-    # type: () -> Dict[str, str]
+def make_go_env(ensure_goroot=True):
+    # type: (bool) -> Dict[str, str]
     # TODO(msolo) Respect args to bzl to use the proper default runtime.
     ws = bazel_utils.find_workspace()
+
+    env = {
+        "GOPATH": os.path.join(bazel_utils.find_workspace(), "go"),
+        "GOCACHE": "/tmp/go_build_cache",
+    }
+
     bazel_ws_root = "bazel-" + os.path.basename(ws)
     GOROOT = os.path.join(
         ws, bazel_ws_root, "external/go_1_12_17_linux_amd64_tar_gz/go"
     )
-    ensure_goroot_exists(GOROOT)
+    if ensure_goroot and not os.path.isdir(GOROOT):
+        _ensure_goroot_exists(GOROOT)
 
-    return {
-        "GOROOT": GOROOT,
-        "GOPATH": os.path.join(bazel_utils.find_workspace(), "go"),
+    if os.path.isdir(GOROOT):
+        env["GOROOT"] = GOROOT
         # go-errcheck behaves incorrectly if $GOROOT/bin is not added to $PATH
-        "PATH": os.path.join(GOROOT, "bin") + os.pathsep + os.getenv("PATH", ""),
-        "GOCACHE": "/tmp/go_build_cache",
-    }
+        env["PATH"] = os.path.join(GOROOT, "bin") + os.pathsep + os.getenv("PATH", "")
+
+    return env
