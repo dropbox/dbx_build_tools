b04559998a06cff4d40d7690e9f507d6219875c8
David Goldstein <dgoldstein@dropbox.com>
2020-09-24 02:13:40 +0000

    Internal change
    GitOrigin-RevId: 89c8a1c28dbcbbfce33d0f210779786e68919791

---
+++ build_tools/npm_utils.py
@@ -0,0 +1,32 @@
+""" This file is separate from build_tools.bazel_utils because it is owned by web-platform
+
+This would be within build_tools/bzl_lib/dbx/ but we need this logic when validating our targets for bzl gen, and the logic for that is in build_tools/bazel_utils.py
+"""
+
+from typing import Optional, Text
+
+
+def target_to_npm_name(target):
+    # type: (Text) -> Optional[Text]
+    if "/npm/" not in target:
+        return None
+
+    end = target.index(":") if ":" in target else len(target)
+
+    npm_name = target[target.index("/npm/") + len("/npm/") : end]
+    if npm_name.startswith("at_"):
+        npm_name = "@" + npm_name[3:]
+
+    if _looks_like_npm_name(npm_name):
+        return npm_name
+    else:
+        return None
+
+
+def _looks_like_npm_name(npm_name):
+    # type: (Text) -> bool
+    """ Names of npm packages are either of the form <foo> or @<foo>/<bar> """
+
+    return (npm_name.startswith("@") and npm_name.count("/") == 1) or (
+        not npm_name.startswith("@") and npm_name.count("/") == 0
+    )
---
+++ build_tools/npm_utils_tests.py
@@ -0,0 +1,32 @@
+from build_tools.npm_utils import target_to_npm_name
+
+
+def test_target_to_npm_name():
+    # type: () -> None
+    """ target_to_npm_name takes the target and computes the name of the npm package
+        that the target should refer to.
+    """
+
+    # basic case
+    assert target_to_npm_name("//npm/angular-cookies") == "angular-cookies"
+
+    # make sure we don't blow up or misgenerate if a :part is present in the target
+    assert (
+        target_to_npm_name("//npm/angular-cookies:angular-cookies") == "angular-cookies"
+    )
+
+    # scoped packages
+    assert target_to_npm_name("//npm/at_types/foo") == "@types/foo"
+
+    # non-root npm folder
+    assert target_to_npm_name("//fakecenter/static/nfs/npm/foo") == "foo"
+
+    # some invalid names - should all return None
+    assert target_to_npm_name("//npm/at_types/react/node_modules") is None
+    assert target_to_npm_name("//npm/foo/bar") is None
+    assert target_to_npm_name("//npm/at_foo") is None
+    assert target_to_npm_name("//npm/at_types") is None
+
+    # a non-npm test case - necessary because we use this function in some situations
+    # where an npm input isn't guaranteed
+    assert target_to_npm_name("//broken") is None
