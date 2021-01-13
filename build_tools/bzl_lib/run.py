b9b91c698e5aac66f50a99140f86af6553384064
Anthony Danducci <adanducci@dropbox.com>
2021-01-12 16:43:21 -0800

    Support changing cwd in run.py.
    GitOrigin-RevId: 58e319ec59a226c8d6fa094fc9044c64c72eac0a

--- build_tools/bzl_lib/run.py
+++ build_tools/bzl_lib/run.py
@@ -1,13 +1,13 @@
 import subprocess
 import sys
 
-from typing import List
+from typing import List, Optional
 
 from build_tools.go.env import make_go_env
 
 
-def run_cmd(cmd, use_go_env=False, verbose=False):
-    # type: (List[str], bool, bool) -> str
+def run_cmd(cmd, use_go_env=False, verbose=False, cwd=None):
+    # type: (List[str], bool, bool, Optional[str]) -> str
     env = dict()
     if use_go_env:
         env = make_go_env()
@@ -16,7 +16,7 @@ def run_cmd(cmd, use_go_env=False, verbose=False):
     if verbose:
         print("exec:", " ".join(env_args + cmd), file=sys.stderr)
     try:
-        output = subprocess.check_output(cmd, env=env, encoding="ascii")
+        output = subprocess.check_output(cmd, env=env, encoding="ascii", cwd=cwd)
     except subprocess.CalledProcessError:
         sys.exit(1)
 
