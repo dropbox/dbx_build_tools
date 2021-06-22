42325dbe5a1dd939e210d6271f2b0c0b0328eda7
Benjamin Peterson <benjamin@dropbox.com>
2021-06-22 18:00:10 +0000

    Revert upstream x/tools 67e49ef2d0f326051e22a4a55bdf9344ae1a8ed8 because it breaks #includes from package directories.
    GitOrigin-RevId: c51bd56975c7870482b424b55d1d915aa68e2030

--- go/src/golang.org/x/tools/go/internal/cgo/cgo.go
+++ go/src/golang.org/x/tools/go/internal/cgo/cgo.go
@@ -57,14 +57,13 @@ import (
 	"go/build"
 	"go/parser"
 	"go/token"
+	exec "golang.org/x/sys/execabs"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
-
-	exec "golang.org/x/sys/execabs"
 )
 
 // ProcessFiles invokes the cgo preprocessor on bp.CgoFiles, parses
@@ -160,13 +159,14 @@ func Run(bp *build.Package, pkgdir, tmpdir string, useabs bool) (files, displayF
 	}
 
 	args := stringList(
-		"go", "tool", "cgo", "-srcdir", pkgdir, "-objdir", tmpdir, cgoflags, "--",
+		"go", "tool", "cgo", "-objdir", tmpdir, cgoflags, "--",
 		cgoCPPFLAGS, cgoexeCFLAGS, cgoFiles,
 	)
 	if false {
-		log.Printf("Running cgo for package %q: %s", bp.ImportPath, args)
+		log.Printf("Running cgo for package %q: %s (dir=%s)", bp.ImportPath, args, pkgdir)
 	}
 	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Dir = pkgdir
 	cmd.Stdout = os.Stderr
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
