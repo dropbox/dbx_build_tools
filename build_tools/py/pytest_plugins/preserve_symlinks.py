b0414cd5086703680e91e58c2364a78721c4f4a3
Jared Hance <jhance@dropbox.com>
2022-02-09 15:00:25 -0800

    [py] The latest pytest
    GitOrigin-RevId: 1664880aeda93aed18d3350d9c0c5cee0015dc51

--- build_tools/py/pytest_plugins/preserve_symlinks.py
+++ build_tools/py/pytest_plugins/preserve_symlinks.py
@@ -4,10 +4,12 @@
 # is bad because it usually escapes the sandbox.  If
 # https://github.com/pytest-dev/pytest/issues/5266 is ever fixed, we can probably get rid of this.
 
-import py
+import pathlib
+
+import attr
 
 
-class _SymlinkPhilicPath(py.path.local):
+class _SymlinkPhilicPath(pathlib.PosixPath):
     def realpath(self):
         # type: () -> _SymlinkPhilicPath
         return self
@@ -17,8 +19,11 @@ def pytest_load_initial_conftests(early_config):
     pm = early_config.pluginmanager
     gctm = pm._getconftestmodules
 
-    def _getconftestmodules(p):
-        return gctm(_SymlinkPhilicPath(p))
+    def _getconftestmodules(anchor, importmode, rootpath):
+        return gctm(_SymlinkPhilicPath(anchor), importmode, rootpath)
 
     pm._getconftestmodules = _getconftestmodules
-    early_config.invocation_dir = _SymlinkPhilicPath(early_config.invocation_dir)
+    early_config.invocation_params = attr.evolve(
+        early_config.invocation_params,
+        dir=_SymlinkPhilicPath(early_config.invocation_params.dir),
+    )
