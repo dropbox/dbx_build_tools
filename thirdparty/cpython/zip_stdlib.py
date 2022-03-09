30b078a3e16854c3f8bb8628fffbba90b2a6a61f
Jared Hance <jhance@dropbox.com>
2022-03-09 10:50:00 -0800

    Remove python2 support from zip_stdlib
    Summary: Flattening six.PY3 does not catch this, since we can't use six here.
    GitOrigin-RevId: cdcd141d213a926032456c47701265695e4b2899

--- thirdparty/cpython/zip_stdlib.py
+++ thirdparty/cpython/zip_stdlib.py
@@ -10,10 +10,6 @@ import sys
 import tempfile
 import zipfile
 
-PY3 = sys.version_info[0] >= 3
-if not PY3:
-    import imp
-
 EPOCH = (2018, 11, 11, 11, 11, 11)
 EPOCH_BIN = struct.pack('<L', calendar.timegm(EPOCH))
 
@@ -33,27 +29,18 @@ def main():
                     source = fp.read()
                     z.writestr(info, source)
                     if inp.endswith(".py"):
-                        if PY3:
-                            tf = tempfile.NamedTemporaryFile()
-                            tf.close()
-                            py_compile.compile(
-                                inp,
-                                cfile=tf.name,
-                                dfile=arcname,
-                                invalidation_mode=py_compile.PycInvalidationMode.UNCHECKED_HASH,
-                            )
-                            with open(tf.name, "rb") as tfh:
-                                # zipped stdlib import doesn't use the __pycache__ paths
-                                info = zipfile.ZipInfo(arcname + "c", EPOCH)
-                                z.writestr(info, tfh.read())
-                        else:
-                            code = compile(source, arcname, 'exec', dont_inherit=True)
-                            data = io.BytesIO()
-                            data.write(imp.get_magic())
-                            data.write(EPOCH_BIN)
-                            data.write(marshal.dumps(code))
+                        tf = tempfile.NamedTemporaryFile()
+                        tf.close()
+                        py_compile.compile(
+                            inp,
+                            cfile=tf.name,
+                            dfile=arcname,
+                            invalidation_mode=py_compile.PycInvalidationMode.UNCHECKED_HASH,
+                        )
+                        with open(tf.name, "rb") as tfh:
+                            # zipped stdlib import doesn't use the __pycache__ paths
                             info = zipfile.ZipInfo(arcname + "c", EPOCH)
-                            z.writestr(info, data.getvalue())
+                            z.writestr(info, tfh.read())
             else:
                 z.write(inp, arcname)
 
